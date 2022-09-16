8a445416af87b7f0a0eaa99270005bd4ae97449b
Sebastian Giannini <109916777+KingMomoGiannini@users.noreply.github.com>
2022-09-16 17:12:28 -0300

    Add files via upload

---
+++ SegndoEj_punteros.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void  intercambio(int *x, int*y){
+    int c;
+    c=*x;
+    *x=*y;
+    *y=c;
+}
+
+int main(){
+    int num1,num2,*a,*b;
+    printf("Ingrese un valor para A: ");
+    scanf("%d",&num1);
+    printf("Ingrese otro valor para B: ");
+    scanf("%d",&num2);
+    a=&num1;
+    b=&num2;
+    intercambio(a,b);
+    printf("El valor de A ahora es %d\n", *a);
+    printf("El valor de B ahora es %d", *b);
+    return 0;
+}
